3955218f8a52924a8a2be8ca8c8266b2c6644794
Tamás Ádám <atomi2500@gmail.com>
2022-11-24 19:09:46 +0200

    remove separators

--- src/main/java/com/vamk/tbg/ui/MainContainer.java
+++ src/main/java/com/vamk/tbg/ui/MainContainer.java
@@ -5,7 +5,6 @@ import com.vamk.tbg.signal.impl.EntityPlaysSignal;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JSeparator;
 import java.awt.Font;
 import java.awt.GridLayout;
 
@@ -21,11 +20,9 @@ public class MainContainer extends JFrame {
         setFocusable(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(700, 500);
-        setLayout(new GridLayout(5,  1));
+        setLayout(new GridLayout(3,  1));
         add(new GameContainer(dispatcher));
-        add(new JSeparator());
         add(this.entityLabel);
-        add(new JSeparator());
         add(new EntityDataContainer(dispatcher));
         setVisible(true);
         dispatcher.subscribe(EntityPlaysSignal.class, this::onEntityPlays);
