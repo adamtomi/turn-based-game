83d94adfe5ed7be8a696af6c4364d716da4cc8e1
Tamás Ádám <atomi2500@gmail.com>
2022-12-08 20:50:48 +0200

    add MapKey

---
+++ src/main/java/com/vamk/tbg/config/MapKey.java
@@ -0,0 +1,65 @@
+package com.vamk.tbg.config;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+public abstract class MapKey<T> extends AbstractConfigKey<Map<String, T>> {
+    /* This value disables size checks */
+    private static final int NONE = -1;
+    /* Separates key-value pairs */
+    private static final String SEPARATOR = ",";
+    /* Separates a single key and value */
+    private static final String KV_SEPARATOR = "=";
+    private final int expectedSize;
+
+    public MapKey(String path, int expectedSize) {
+        super(path);
+        this.expectedSize = expectedSize;
+    }
+
+    public MapKey(String path) {
+        this(path, NONE);
+    }
+
+    @Override
+    public Map<String, T> map(String value) {
+        List<String> values = Arrays.stream(value.split(SEPARATOR)).map(String::trim)
+                .toList();
+
+        int size = values.size();
+        if (size != this.expectedSize && this.expectedSize != NONE) throw new IllegalArgumentException("Expected %d elements, got %d".formatted(this.expectedSize, size));
+
+        Map<String, T> result = new HashMap<>();
+        for (String each : values) {
+            String[] data = each.split(KV_SEPARATOR, 2);
+            if (data.length != 2) throw new IllegalArgumentException("Invalid map entry %s at %s".formatted(each, getPath()));
+
+            result.put(data[0].trim(), mapSingle(data[1].trim()));
+        }
+
+        return result;
+    }
+
+    public abstract T mapSingle(String value);
+
+    public static final class Simple<T> extends MapKey<T> {
+        private final Function<String, T> mapper;
+
+        public Simple(String path, int expectedSize, Function<String, T> mapper) {
+            super(path, expectedSize);
+            this.mapper = mapper;
+        }
+
+        public Simple(String path, Function<String, T> mapper) {
+            this(path, NONE, mapper);
+        }
+
+        @Override
+        public T mapSingle(String value) {
+            return this.mapper.apply(value);
+        }
+    }
+}
